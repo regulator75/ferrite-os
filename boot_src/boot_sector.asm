9546daaa8912f25e0f565d68a0c11ac5ad5823a5
Viktor Mårtensson <viktor.martensson@gmail.com>
2020-10-01 23:15:58 -0700

    Load more of kernel from disc to accomodate for increased binary

--- boot_src/boot_sector.asm
+++ boot_src/boot_sector.asm
@@ -41,7 +41,7 @@ load_kernel:
     mov bx, MSG_LOADINGKERNEL
     call print
     mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
-    mov dh, 8
+    mov dh, 16
     mov dl, [BOOT_DRIVE]
     call disk_load
     
