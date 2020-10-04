4f7f3b417538fbe6db682e7ef46e856a948c4c91
Viktor Mårtensson <viktor.martensson@gmail.com>
2020-10-03 17:12:11 -0700

    Bugfix in struct to alight with what is actually pushed in the IRQ and Interrupt handlers

--- boot_src/interrupts.h
+++ boot_src/interrupts.h
@@ -1,8 +1,9 @@
 #include "types.h"
 typedef struct {
    uint64_t ds; /* Data segment selector */
-   uint64_t rdi, rsi, rbp, rsp, rbx, rdx, rcx, rax; // pushaq
-   uint64_t int_no, err_code; /* Interrupt number and error code (if applicable) */
+   uint64_t rdi, rsi, rbp, rbx, rdx, rcx, rax; // pushaq
+   uint64_t int_no; // Interrupt gate. 0-31 are the common projected interrupts. 32-48 are IRQ 0-15
+   uint64_t code; /* In case of an interrupt, possibly an error code. In case of IRQ, the IRQ number*/
    uint64_t eip, cs, eflags, useresp, ss; /* Pushed by the processor automatically */
 } isr_irq_handler_parameters;
 
