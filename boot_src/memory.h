ebfdc41a5b10be8431fcea93864ed091322073fc
Viktor Mårtensson <viktor.martensson@gmail.com>
2020-10-05 21:40:28 -0700

    Declarations for new functions in memory.c, including loading memory layout

--- boot_src/memory.h
+++ boot_src/memory.h
@@ -1,7 +1,10 @@
 #ifndef MEMORY_MGNT_H
 #define MEMORY_MGNT_H
 
+void memory_analyze_and_print();
+
 void memory_copy(const char *source, char *dest, int nbytes);
+void memory_clear( char * target, uint64_t size);
 
 
 #endif
